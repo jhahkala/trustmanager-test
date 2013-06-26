0eb07650ae2f56fddaa472c2cc411ddbfb3831b2
jhahkala <joni.hahkala@cern.ch>
2013-06-26 13:13:11 +0200

    add the sl5 script avid asking confirmation on sl6 script

--- testsuite/trustmanager.testing.emi2.sh
+++ testsuite/trustmanager.testing.emi2.sh
@@ -28,20 +28,17 @@ do
       rm -f $xfile
   fi
 done
-
 wget http://repository.egi.eu/sw/production/cas/1/current/repo-files/EGI-trustanchors.repo
 
-cd
 
+cd 
 yum install -y yum-priorities yum-protectbase
 
 rpm --import http://emisoft.web.cern.ch/emisoft/dist/EMI/2/RPM-GPG-KEY-emi
-rpm -ivh http://emisoft.web.cern.ch/emisoft/dist/EMI/2/sl6/x86_64/base/emi-release-2.0.0-1.sl6.noarch.rpm
+rpm -ivh http://emisoft.web.cern.ch/emisoft/dist/EMI/2/sl5/x86_64/base/emi-release-2.0.0-1.sl5.noarch.rpm
 
-#get trustmanager 3.0.3
-wget https://github.com/jhahkala/trustmanager/blob/gh-pages/packages/emi-trustmanager-3.1.5-1.sl6.noarch.rpm?raw=true
+wget https://github.com/jhahkala/trustmanager/blob/gh-pages/packages/emi-trustmanager-3.1.5-1.sl5.noarch.rpm?raw=true
 
-cd 
 # Gotta make sure the EPEL repository is enabled!
 #sed -i 's/\/EMI\/1\/sl/\/EMI\/2\/RC\/sl/g' /etc/yum.repos.d/emi1-base.repo
 #sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/emi1-base.repo
@@ -52,53 +49,33 @@ cd
 
 yum install -y emi-trustmanager-3.1.5-1.sl6.noarch.rpm
 
-install_list="emi-trustmanager-axis emi-trustmanager-tomcat 
-emi-trustmanager-test glite-yaim-core fetch-crl ca-policy-egi-core tomcat5 
-git xml-commons-apis emacs"
-
-# emacs??
-echo yum install -y $install_list
-
-    yum install -y $install_list |tee yum.log; result=$?
-    if [ $result -ne 0 ]
-    then
-        echo "\n \n ERROR installing $target \n \n";
-	exit 1;
-    else 
-        echo "Installed $target with return code $result."
-    fi
-
-
-# yum install -y emi-trustmanager
-# yum install -y emi-trustmanager-axis 
-# yum install -y emi-trustmanager-tomcat 
-# yum install -y emi-trustmanager-test 
-# yum install -y glite-yaim-core 
-# yum install -y fetch-crl 
-# yum install -y ca-policy-egi-core 
-# yum install -y tomcat5 
-# yum install -y cvs 
-# yum install -y xml-commons-apis 
-# yum install -y emacs
+CMD="yum install -y emi-trustmanager-axis emi-trustmanager-tomcat emi-trustmanager-test glite-yaim-core fetch-crl ca-policy-egi-core tomcat5 cvs xml-commons-apis emacs"
+echo $CMD; $CMD
+if [ $result -ne 0 ] ; then
+    echo "\n \n ERROR installing packages \n \n";
+    exit 1;
+else 
+    echo "Installed packages with return code $result."
+fi
 
 cd ~
 #cp /opt/glite/yaim/examples/siteinfo/site-info.def .
 
 # config default with yaim 
 echo "#" >site-info.def
-echo y|/opt/glite/yaim/bin/yaim -r -s site-info.def -f config_secure_tomcat|tee yaim.log
+/opt/glite/yaim/bin/yaim -r -s site-info.def -f config_secure_tomcat
 
 # add override for server credential expiration check
-cp /etc/tomcat6/server.xml /etc/tomcat6/server.xml.sed
-cat /etc/tomcat6/server.xml.sed | sed "s/secure=\"true\"/secure=\"true\" internalOverrideExpirationCheck=\"true\"/" >/etc/tomcat6/server.xml
+cp /etc/tomcat5/server.xml /etc/tomcat5/server.xml.sed
+cat /etc/tomcat5/server.xml.sed | sed "s/secure=\"true\"/secure=\"true\" internalOverrideExpirationCheck=\"true\"/" >/etc/tomcat5/server.xml
 
 #update crls
 echo updating CRLs....
 /usr/sbin/fetch-crl
 
 #clean up tomcat logs
-/sbin/service tomcat6 stop
-rm -f /var/log/tomcat6/*
+/sbin/service tomcat5 stop
+rm -f /var/log/tomcat5/*
 
 # check out the test cert generation stuff and generate test certs
 #export CVSROOT=":pserver:anonymous@glite.cvs.cern.ch:/cvs/glite"
@@ -109,11 +86,12 @@ cd test-certs
 bin/generate-test-certificates.sh --all --voms /root/certs
 cd ~
 
-cp /usr/share/java/trustmanager-test.war /var/lib/tomcat6/webapps
+cp /usr/share/java/trustmanager-test.war /var/lib/tomcat5/webapps
 
 cd trustmanager-test/testsuite/tests
 ./test-setup.sh --certdir /root/certs/
 
+/sbin/service tomcat5 start
 sleep 15
 
 echo "#run following commands:"
@@ -150,7 +128,7 @@ fi
 echo #set clock forward to make CRLs expire
 date --set='+70 minutes'
 date --set='+70 minutes'
-sleep 15
+sleep 30
 ./certificate-tests+1h.sh --certdir /root/certs/
 RES=$?
 if [ $RES -ne 0 ]; then
@@ -163,3 +141,4 @@ if [ $RES -ne 0 ]; then
     echo client +1h tests failed
     exit 1
 fi
+
